22f0b1a9e22e790065dad03498a034e3ff4ff361
gdavid2 <124572489+gdavid2@users.noreply.github.com>
2023-02-23 11:40:51 +0100

    interrupt

--- projects/esn7/counter_decounter/cpt_dcpt.vhd
+++ projects/esn7/counter_decounter/cpt_dcpt.vhd
@@ -6,12 +6,14 @@ entity cpt_dcpt is
 	port (clk: in std_logic;
          clr: in std_logic;
 			cpt_dcpt: in std_logic;
-			cpt: out std_logic_vector(15 downto 0));
+			cpt: out std_logic_vector(15 downto 0);
+			end_cycle_irq: out std_logic);
 end entity;
 
 architecture seq of cpt_dcpt is
 	
 	signal cpt_int: std_logic_vector(15 downto 0);
+	signal end_cycle_irq_int: std_logic;
 	
 	type t_state is (COMPTAGE,DECOMPTAGE);
 	signal state: t_state; 
@@ -21,6 +23,7 @@ begin
 	begin
 		if (clr= '1') then
 			cpt_int <= "0000000000000000";
+			end_cycle_irq_int <= '0';
 			state <= COMPTAGE;
 			
 		elsif cpt_dcpt= '1' and rising_edge(clk) then
@@ -31,6 +34,8 @@ begin
 			case state is
 			
 			when COMPTAGE =>
+				end_cycle_irq_int <= '0';
+				
 				if cpt_int = "1111111111111111" then
 					cpt_int <= cpt_int - 1;
 					state <= DECOMPTAGE;
@@ -41,6 +46,7 @@ begin
 			when DECOMPTAGE =>
 				if cpt_int = "0000000000000000" then
 					cpt_int <= cpt_int + 1;
+					end_cycle_irq_int <= '1';
 					state <= COMPTAGE;
 				else
 					cpt_int <= cpt_int - 1;
@@ -51,5 +57,6 @@ begin
 	end process;
 
 	cpt <= cpt_int;
+	end_cycle_irq <= end_cycle_irq_int;
 	
 end architecture;
